8e3b7961a7140795a832c9ee6df0c45acbc3a3ef
SujayBonde <sujaybonde2005@gmail.com>
2025-12-25 14:04:21 +0530

    Initial commit of MERN stack project

--- frontend/src/context/transactionService.js
+++ frontend/src/context/transactionService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/transactions/'
+const API_URL = 'https://expense-tracker-mern-backend-two.vercel.app/api/transactions/'
 
 // Create new transaction
 const createTransaction = async (transactionData, token) => {
